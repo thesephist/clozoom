f31569320ec4e18a5ca5932b4698323aabb0454e
Rounak Datta <rounakdatta12@gmail.com>
2022-03-22 21:22:21 +0530

    Add  support for controlling close timeout

--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
 const input = document.querySelector('input');
+const timeout = document.getElementById('timeout')
 
 // set checkbox value
-chrome.storage.local.get({enabled: true}, result => {
+chrome.storage.local.get({'enabled': true}, result => {
     input.checked = result.enabled;
+});
+
+chrome.storage.local.get({'secondsToClose': 5}, result => {
+    timeout.value = result.secondsToClose;
 });
 
 // trigger toggle
 input.addEventListener('change', evt => {
     const enabled = evt.target.checked;
-    chrome.storage.local.set({enabled});
+    chrome.storage.local.set({'enabled': enabled});
+});
+
+timeout.addEventListener('change', tmt => {
+    const secondsToClose = tmt.target.value;
+    chrome.storage.local.set({'secondsToClose': secondsToClose});
 });
 
